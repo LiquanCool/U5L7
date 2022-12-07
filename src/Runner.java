a6aa2f7482cd7d879566b86f0b456b2976f31d54
liquancool <llei0111@bths.edu>
2022-12-07 18:08:52 -0500

    refactored main

--- src/Runner.java
+++ src/Runner.java
@@ -1,40 +1,25 @@
 public class Runner
 {
+    public static void convert(double miles, double km)
+    {
+        System.out.println(RaceUtility.milesToKm(miles));
+        System.out.println(RaceUtility.kmToMiles(km));
+    }
+
+    public static void capitalize(String str)
+    {
+        System.out.println(RaceUtility.makeProper(str));
+    }
     public static void main(String[] args)
     {
-        double miles1 = 13.85;
-        double km1 = 40;
-        System.out.println(RaceUtility.milesToKm(miles1));
-        System.out.println(RaceUtility.kmToMiles(km1));
-
-        double miles2 = 26.42;
-        double km2 = 3.5;
-        System.out.println(RaceUtility.milesToKm(miles2));
-        System.out.println(RaceUtility.kmToMiles(km2));
-
-        double miles3 = 5;
-        double km3 = 20.50;
-        System.out.println(RaceUtility.milesToKm(miles3));
-        System.out.println(RaceUtility.kmToMiles(km3));
-
-        double miles4 = 0;
-        double km4 = 0;
-        System.out.println(RaceUtility.milesToKm(miles4));
-        System.out.println(RaceUtility.kmToMiles(km4));
-
-        String title = "welcome to the marathon!";
-        System.out.println(RaceUtility.makeProper(title));
-
-        title = "It's TIME for THE 5k!";
-        System.out.println(RaceUtility.makeProper(title));
-
-        title = "runners, take your marks...";
-        System.out.println(RaceUtility.makeProper(title));
-
-        title = "2 MILES TO GO!";
-        System.out.println(RaceUtility.makeProper(title));
-
-        title = "water break in 0.5 miles";
-        System.out.println(RaceUtility.makeProper(title));
+        convert(13.85,40);
+        convert(26.42,3.5);
+        convert(5, 20.50);
+        convert(0,0);
+        capitalize("welcome to the marathon!");
+        capitalize("It's TIME for THE 5k!");
+        capitalize("runners, take your marks...");
+        capitalize("2 MILES TO GO!");
+        capitalize("water break in 0.5 miles");
     }
 }
